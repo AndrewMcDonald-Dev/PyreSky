aec04ed728121b25ac25cd47f55a3c7d1c5d9d4d
Andrew McDonald <andrewmcdonald.dev@gmail.com>
2025-02-14 01:20:14 -0500

    Templating working. Almost have htmx hooked up for full display of incoming message.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
-use axum::routing::get;
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Extension,
+};
 use firesky::{on_connect, send_message_on_receive};
+use lazy_static::lazy_static;
 use socketioxide::SocketIo;
+use tera::Tera;
 use tokio::net::TcpListener;
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let source = "templates/**/*.html";
+        Tera::new(source).unwrap()
+    };
+}
+
+async fn handler() -> impl IntoResponse {
+    let page_content = TEMPLATES
+        .render("index.html", &tera::Context::new())
+        .unwrap();
+
+    Html(page_content)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Needed for websockets
     rustls::crypto::ring::default_provider()
         .install_default()
         .expect("Failed to install rustls crypto provider.");
 
+    // Lgging
     tracing::subscriber::set_global_default(FmtSubscriber::default())?;
 
     // Biuld SocketIO layer
@@ -20,16 +43,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     io.ns("/", on_connect);
 
-    tokio::spawn(send_message_on_receive(io));
+    tokio::spawn(send_message_on_receive(io, TEMPLATES.clone()));
 
     // Build Axum app
-    let app = axum::Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .layer(
-            ServiceBuilder::new()
-                .layer(CorsLayer::permissive())
-                .layer(layer),
-        );
+    let app = axum::Router::new().route("/", get(handler)).layer(
+        ServiceBuilder::new()
+            .layer(CorsLayer::permissive())
+            .layer(layer)
+            .layer(Extension(TEMPLATES.clone())),
+    );
 
     // Start server
     info!("Starting server...");
--- src/websocket.rs
+++ src/websocket.rs
@@ -2,11 +2,13 @@ use futures_util::StreamExt;
 use socketioxide::{extract::SocketRef, SocketIo};
 use tracing::{debug, error, info};
 
+use crate::{Kind, Operation};
+
 pub async fn on_connect(socket: SocketRef) {
     info!("Socket connected: {}", socket.id);
 }
 
-pub async fn send_message_on_receive(io: SocketIo) {
+pub async fn send_message_on_receive(io: SocketIo, templates: tera::Tera) {
     let url =
         "wss://jetstream1.us-east.bsky.network/subscribe?wantedCollections=app.bsky.feed.post";
 
@@ -19,7 +21,22 @@ pub async fn send_message_on_receive(io: SocketIo) {
             Ok(msg) => {
                 debug!("Received message: {:?}", msg);
 
-                io.broadcast().emit("message", &msg).await.unwrap();
+                if let Kind::Commit { commit } = msg.kind {
+                    if let Operation::Create {
+                        record,
+                        collection: _,
+                        rkey: _,
+                        cid: _,
+                    } = commit.operation
+                    {
+                        let mut context = tera::Context::new();
+                        context.insert("message", &record.text);
+
+                        let message = templates.render("message.html", &context).unwrap();
+
+                        io.emit("message", &message).await.unwrap();
+                    }
+                }
             }
             Err(e) => {
                 error!("Error: {}", e);
